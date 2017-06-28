4f0b64954949d6205f36f937033a5745a028ea0e
Anton Goncharuk <antonge92@gmail.com>
2017-06-28 13:24:24 +0300

    Install active admin panel (admin user model, 2 migration files,  dashboard, 2 initializers for devise and aa, devise locale file, update routes, update schema and seeds).

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'antonge92@gmail.com', password: '1qw23e', password_confirmation: '1qw23e')

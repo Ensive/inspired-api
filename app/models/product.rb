b8d62deb42632e9ba3793060f0fe90deba482c7f
Anton Goncharuk <antonge92@gmail.com>
2017-06-29 12:21:58 +0300

    Provide validates rules for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,14 @@ class Product < ApplicationRecord
 
   # before_destroy :ensure_not_referenced
 
+  validates :sku, :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to:  0.01 }
+  validates :title, uniqueness: true
+  validates :image, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL should reference to the image'
+  }
+
   def to_s
     title
   end
